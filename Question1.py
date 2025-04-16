813bc6c3be3eb6a9c2ae45f612c854592e78137a
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-04-16 17:10:50 +0530

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,36 +1,43 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
 
-class CustomCNN(nn.Module):
-    def __init__(self, in_channels=3, num_classes=10,
-                 conv_filters=[32, 64, 128, 256, 512],
-                 filter_size=3,
-                 activation_fn=nn.ReLU,
-                 dense_neurons=256):
-        super(CustomCNN, self).__init__()
+class CNNModel(nn.Module):
+    def __init__(self, num_filters=32, kernel_size=3, num_dense_neurons=128):
+        super(CNNModel, self).__init__()
         
-        self.conv_layers = nn.Sequential()
-        current_in_channels = in_channels
+        self.conv1 = nn.Conv2d(3, num_filters, kernel_size=kernel_size, padding=1)
+        self.conv2 = nn.Conv2d(num_filters, num_filters*2, kernel_size=kernel_size, padding=1)
+        self.conv3 = nn.Conv2d(num_filters*2, num_filters*4, kernel_size=kernel_size, padding=1)
+        self.conv4 = nn.Conv2d(num_filters*4, num_filters*8, kernel_size=kernel_size, padding=1)
+        self.conv5 = nn.Conv2d(num_filters*8, num_filters*16, kernel_size=kernel_size, padding=1)
+        
+        self.pool = nn.MaxPool2d(2, 2)  # Max-pooling layer with 2x2 window
+        
+        self.fc1 = nn.Linear(num_filters*16*8*8, num_dense_neurons)  # Adjust input features after flattening
+        self.fc2 = nn.Linear(num_dense_neurons, 10)  # Output layer with 10 classes
 
-        for i, out_channels in enumerate(conv_filters):
-            self.conv_layers.add_module(f'conv{i+1}', nn.Conv2d(current_in_channels, out_channels, kernel_size=filter_size, padding=1))
-            self.conv_layers.add_module(f'act{i+1}', activation_fn())
-            self.conv_layers.add_module(f'pool{i+1}', nn.MaxPool2d(kernel_size=2))
-            current_in_channels = out_channels
-
-        self.flatten = nn.Flatten()
-
-        # Assume input image 224x224, compute final size
-        final_size = 224 // (2 ** 5)  # 5 pool layers halve it 5 times
-        self.feature_dim = conv_filters[-1] * final_size * final_size
-
-        self.fc1 = nn.Linear(self.feature_dim, dense_neurons)
-        self.out = nn.Linear(dense_neurons, num_classes)
+        self.relu = nn.ReLU()  # ReLU activation function
 
     def forward(self, x):
-        x = self.conv_layers(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.out(x)
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        
+        x = self.relu(self.conv3(x))
+        x = self.pool(x)
+        
+        x = self.relu(self.conv4(x))
+        x = self.pool(x)
+        
+        x = self.relu(self.conv5(x))
+        x = self.pool(x)
+        
+        x = x.view(x.size(0), -1)  # Flatten the tensor for the fully connected layer
+        
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)  # Output layer
+        
         return x
